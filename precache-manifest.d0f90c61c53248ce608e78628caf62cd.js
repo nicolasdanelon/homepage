d74d5e04e439e82976067034f179541da2610501
Nicolas Danelon <nicolas.danelon@softvision.com>
2020-01-17 11:35:40 -0300

    Updates

---
+++ precache-manifest.d0f90c61c53248ce608e78628caf62cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0acc3d45b8574a90fcaa70f997a6a723",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "9e8c1cb82f8a7676dd74",
+    "url": "/homepage/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ac65d5ab9627f9f8d65d",
+    "url": "/homepage/static/js/2.b2c95304.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/homepage/static/js/2.b2c95304.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e8c1cb82f8a7676dd74",
+    "url": "/homepage/static/js/main.04b90883.chunk.js"
+  },
+  {
+    "revision": "1376ffcfdf543a7e02fe",
+    "url": "/homepage/static/js/runtime-main.c048e2d4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/homepage/static/media/logo.5d5d9eef.svg"
+  }
+]);
